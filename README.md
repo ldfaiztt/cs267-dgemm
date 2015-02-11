120b2366dc3cd59cc43455b03a20cf53b0da5caa
Xian Shi <xshi@hopper07.(none)>
2015-02-10 23:19:27 -0800

    Optimization #2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cs267-hw
+# dgemm-tuned-a: Optimization - 1, change the order of the loops (i,j,k); changes which matrix stays in memory and the type of product. No much improvement.
+# dgemm-tuned-b: Optimization - 2, change the order of the loops, Blocking for L1 cache (64), unrolling the loops (4), copy optimization. Speed goes to ~1500, Percentage goes to ~13%
--- src/dgemm-tuned-a.c
+++ src/dgemm-tuned-a.c
@@ -7,21 +7,28 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double* A, double* B, double* C)
 {
   double A1[n];
-  /* For each row i of A */
+  /*  double B1[n];
+   * For each row i of A */
   for (int i = 0; i < n; ++i)
 {
-    for (int m = 0; m < n; m++)
+    for (int m = 0; m < n; ++m)
     {
      A1[m] = A[i+m*n];
     }   
     /* For each column j of B */
     for (int j = 0; j < n; ++j) 
     {
-      /* Compute C(i,j) */
+     /*  for (int m = 0; m < n; ++m)
+      *    {
+      *    B1[m] = B[m+j*n];
+      *    }
+      * Compute C(i,j) */
       double cij = C[i+j*n];
       for( int k = 0; k < n; k++ )
+      {
 	cij += A1[k] * B[k+j*n];
-      C[i+j*n] = cij;
+        C[i+j*n] = cij;
+      }
     }
 }
 }
