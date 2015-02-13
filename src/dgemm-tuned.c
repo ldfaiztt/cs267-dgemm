50d64f84dbf815f33caf2759c7d024e023c91229
Chang Lan <clan@eecs.berkeley.edu>
2015-02-13 02:06:10 -0800

    Final Optimization
    3 levels of blocking, 4x4 SSE SIMD multiplication, transpose

--- src/dgemm-tuned.c
+++ src/dgemm-tuned.c
@@ -1,16 +1,13 @@
+#include <mmintrin.h>
+#include <xmmintrin.h>
+#include <pmmintrin.h>
 #include <emmintrin.h>
 #include <string.h>
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
-#define RSIZE_M 2
-#define RSIZE_K 2
-#define RSIZE_N 2
-#define I_STRIDE 2
-  
-#define BLOCK_ROW 222 
-#define BLOCK_COL 12 
-#define BLOCK_INNER 222 
+#define BLOCK_L1 256
+#define BLOCK_L2 512
 
 #define turn_even(x) (((x) & 1) ? (x+1) : (x))
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -18,111 +15,186 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
 
-static inline void do_block(int M, int K, int N, double* restrict A, double* restrict B, double* restrict C) 
+#define ARRAY(A,i,j) (A)[(j)*lda + (i)]
+
+static inline void calc_4x4(int lda, int K, double* a, double* b, double* c)
 {
-  __m128d c0, c1, a0, a1, b0, b1, b2, b3, d0, d1;    
+  __m128d a0x_1x, a2x_3x, 
+    bx0, bx1, bx2, bx3, 
+    c00_10, c20_30, 
+    c01_11, c21_31,
+    c02_12, c22_32,
+    c03_13, c23_33;
 
-  for (int j=0; j<N; j+=RSIZE_N) 
+  double* c01_11_ptr = c + lda;
+  double* c02_12_ptr = c01_11_ptr + lda;
+  double* c03_13_ptr = c02_12_ptr + lda;
+
+  c00_10 = _mm_loadu_pd(c);
+  c20_30 = _mm_loadu_pd(c+2);
+  c01_11 = _mm_loadu_pd(c01_11_ptr);
+  c21_31 = _mm_loadu_pd(c01_11_ptr+2);
+  c02_12 = _mm_loadu_pd(c02_12_ptr);
+  c22_32 = _mm_loadu_pd(c02_12_ptr+2);
+  c03_13 = _mm_loadu_pd(c03_13_ptr);
+  c23_33 = _mm_loadu_pd(c03_13_ptr+2);
+
+  for (int x = 0; x < K; ++x) 
   {
-    for (int k=0; k<K; k+=RSIZE_K) 
-    {
-      b0 = _mm_load1_pd(B+k+j*K);
-      b1 = _mm_load1_pd(B+k+1+j*K);
-      b2 = _mm_load1_pd(B+k+(j+1)*K);
-      b3 = _mm_load1_pd(B+k+1+(j+1)*K);
-      for (int i=0; i<M; i+=RSIZE_M) 
-      {
-        a0 = _mm_load_pd(A+i+k*M);
-        a1 = _mm_load_pd(A+i+(k+1)*M);
+    a0x_1x = _mm_load_pd(a);
+    a2x_3x = _mm_load_pd(a+2);
+    a += 4;
 
-        c0 = _mm_load_pd(C+i+j*M);
-        c1 = _mm_load_pd(C+i+(j+1)*M);
+    bx0 = _mm_loaddup_pd(b++);
+    bx1 = _mm_loaddup_pd(b++);
+    bx2 = _mm_loaddup_pd(b++);
+    bx3 = _mm_loaddup_pd(b++);
 
-        d0 = _mm_add_pd(c0, _mm_mul_pd(a0,b0));
-        d1 = _mm_add_pd(c1, _mm_mul_pd(a0,b2));
-        c0 = _mm_add_pd(d0, _mm_mul_pd(a1,b1));
-        c1 = _mm_add_pd(d1, _mm_mul_pd(a1,b3));
+    c00_10 = _mm_add_pd(c00_10, _mm_mul_pd(a0x_1x, bx0));
+    c20_30 = _mm_add_pd(c20_30, _mm_mul_pd(a2x_3x, bx0));
+    c01_11 = _mm_add_pd(c01_11, _mm_mul_pd(a0x_1x, bx1));
+    c21_31 = _mm_add_pd(c21_31, _mm_mul_pd(a2x_3x, bx1));
+    c02_12 = _mm_add_pd(c02_12, _mm_mul_pd(a0x_1x, bx2));
+    c22_32 = _mm_add_pd(c22_32, _mm_mul_pd(a2x_3x, bx2));
+    c03_13 = _mm_add_pd(c03_13, _mm_mul_pd(a0x_1x, bx3));
+    c23_33 = _mm_add_pd(c23_33, _mm_mul_pd(a2x_3x, bx3));
+  }
 
-        _mm_store_pd(C+i+j*M,c0);
-        _mm_store_pd(C+i+(j+1)*M,c1); 
-      }
-    }
+  _mm_storeu_pd(c, c00_10);
+  _mm_storeu_pd((c+2), c20_30);
+  _mm_storeu_pd(c01_11_ptr, c01_11);
+  _mm_storeu_pd((c01_11_ptr+2), c21_31);
+  _mm_storeu_pd(c02_12_ptr, c02_12);
+  _mm_storeu_pd((c02_12_ptr+2), c22_32);
+  _mm_storeu_pd(c03_13_ptr, c03_13);
+  _mm_storeu_pd((c03_13_ptr+2), c23_33);
+}
+
+static inline void copy_a (int lda, const int K, double* a_src, double* a_dest) {
+  /* For each 4xK block-row of A */
+  for (int i = 0; i < K; ++i) 
+  {
+    *a_dest++ = *a_src;
+    *a_dest++ = *(a_src+1);
+    *a_dest++ = *(a_src+2);
+    *a_dest++ = *(a_src+3);
+    a_src += lda;
   }
 }
 
-static inline void copy_block(int lda, int M, int N, double* restrict A, double* restrict new_A) 
-{
-  int M_even = turn_even(M);
-  int N_even = turn_even(N);
+static inline void copy_b (int lda, const int K, double* b_src, double* b_dest) {
+  double *b_ptr0, *b_ptr1, *b_ptr2, *b_ptr3;
+  b_ptr0 = b_src;
+  b_ptr1 = b_ptr0 + lda;
+  b_ptr2 = b_ptr1 + lda;
+  b_ptr3 = b_ptr2 + lda;
 
-  for (int j=0; j<N; j++) 
+  for (int i = 0; i < K; ++i) 
   {
-    int new_A_idx = j * M_even;
-    int A_idx = j * lda;
-    memcpy(&new_A[new_A_idx], &A[A_idx], M*sizeof(double));
+    *b_dest++ = *b_ptr0++;
+    *b_dest++ = *b_ptr1++;
+    *b_dest++ = *b_ptr2++;
+    *b_dest++ = *b_ptr3++;
   }
-  if (N % 2) 
-  {
-    int idx = (N_even-1) * M_even;
-    new_A += idx;
-    memset(new_A, 0, M_even * sizeof(double));
-  } 
 }
-
-static inline void add_block(double* new_A, double*  A, int M, int N, int lda, int M_even) 
+/* This auxiliary subroutine performs a smaller dgemm operation
+ *  C := C + A * B
+ * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+static inline void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
-  __m128d a; 
-  int i_step;
-  for (int j=0; j<N; j++) 
+  double A_block[M*K], B_block[K*N];
+  double *a_ptr, *b_ptr, *c;
+
+  const int Nmax = N-3;
+  int Mmax = M-3;
+  int fringe1 = M%4;
+  int fringe2 = N%4;
+
+  int i, j, p;
+
+  /* For each column of B */
+  for (j = 0 ; j < Nmax; j += 4) 
   {
-    for (int i=0; i<M; i+=I_STRIDE) 
-    {
-      i_step = min(I_STRIDE,M-i); 
-      if (unlikely(i_step == 1))
-      {
-        A[i+j*lda] = new_A[i+j*M_even];
-      } 
-      else 
-      {
-        a = _mm_load_pd(new_A + i + j*M_even);
-        _mm_storeu_pd(A+i+j*lda,a);
-      }
+    b_ptr = &B_block[j*K];
+    // copy and transpose B_block
+    copy_b(lda, K, B + j*lda, b_ptr);
+    /* For each row of A */
+    for (i = 0; i < Mmax; i += 4) {
+      a_ptr = &A_block[i*K];
+      if (j == 0) copy_a(lda, K, A + i, a_ptr);
+      c = C + i + j*lda;
+      calc_4x4(lda, K, a_ptr, b_ptr, c);
     }
+  }
+
+  /* Handle "fringes" */
+  if (fringe1 != 0) 
+  {
+    /* For each row of A */
+    for ( ; i < M; ++i)
+      /* For each column of B */ 
+      for (p=0; p < N; ++p) 
+      {
+        /* Compute C[i,j] */
+        double cip = ARRAY(C,i,p);
+        for (int k = 0; k < K; ++k)
+          cip += ARRAY(A,i,k) * ARRAY(B,k,p);
+        ARRAY(C,i,p) = cip;
+      }
+  }
+  if (fringe2 != 0) 
+  {
+    Mmax = M - fringe1;
+    /* For each column of B */
+    for ( ; j < N; ++j)
+      /* For each row of A */ 
+      for (i=0; i < Mmax; ++i) 
+      {
+        /* Compute C[i,j] */
+        double cij = ARRAY(C,i,j);
+        for (int k = 0; k < K; ++k)
+          cij += ARRAY(A,i,k) * ARRAY(B,k,j);
+        ARRAY(C,i,j) = cij;
+      }
   }
 }
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
- * On exit, A and B maintain their input values. */  
-void square_dgemm(int lda, double* A, double* B, double* C)
+ * On exit, A and B maintain their input values. 
+ * Optimization: Two levels of blocking. */  
+void square_dgemm (int lda, double* A, double* B, double* C)
 {
-  int M_even, K_even, N_even;
-
-  double new_A[BLOCK_ROW * BLOCK_INNER] __attribute__((aligned(16)));
-  double new_B[BLOCK_INNER * turn_even(lda)] __attribute__((aligned(16)));
-  double new_C[BLOCK_ROW * BLOCK_COL] __attribute__((aligned(16)));
-
-  for (int k = 0; k < lda; k += BLOCK_INNER) 
+  for (int t = 0; t < lda; t += BLOCK_L2) 
   {
-    int K = min(BLOCK_INNER, lda-k);
-    copy_block(lda, K, lda, B+k, new_B);
-    K_even = turn_even(K);
-
-    for (int i = 0; i < lda; i += BLOCK_ROW) 
+    int end_k = t + min(BLOCK_L2, lda-t);
+    /* For each L2-sized block-column of B */
+    for (int s = 0; s < lda; s += BLOCK_L2) 
     {
-      int M = min (BLOCK_ROW, lda-i);
-      copy_block(lda, M, K, A+i+k*lda, new_A);
-      M_even = turn_even(M);
-
-      for (int j = 0; j < lda; j += BLOCK_COL) 
+      int end_j = s + min(BLOCK_L2, lda-s);
+      /* For each L2-sized block-row of A */ 
+      for (int r = 0; r < lda; r += BLOCK_L2) 
       {
-        int N = min (BLOCK_COL, lda-j);
-        N_even = turn_even(N);               
-        copy_block(lda, M, N, C+i+j*lda, new_C);
-
-        do_block(M_even, K_even, N_even, new_A, new_B+j*K_even, new_C);
-        add_block(new_C, C+i+j*lda, M, N, lda, M_even);
+        int end_i = r + min(BLOCK_L2, lda-r);
+        for (int k = t; k < end_k; k += BLOCK_L1) 
+        {
+          int K = min(BLOCK_L1, end_k-k);
+          /* For each L1-sized block-column of B */
+          for (int j = s; j < end_j; j += BLOCK_L1) 
+          {
+            int N = min(BLOCK_L1, end_j-j);
+            /* For each L1-sized block-row of A */ 
+            for (int i = r; i < end_i; i += BLOCK_L1) 
+            {
+              int M = min(BLOCK_L1, end_i-i);
+              /* Performs a smaller dgemm operation
+               *  C' := C' + A' * B'
+               * where C' is M-by-N, A' is M-by-K, and B' is K-by-N. */
+              do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+            }
+          }
+        }
       }
     }
   }
